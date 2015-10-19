4a23108deeb44001437893f2f189e9afbf0e84e2
Fred Jaworski <fjaworski91@live.com>
2015-10-19 14:32:37 -0700

    Finished activity
    All lines, text, ovals and images are drawing correctly to the screen.

--- app/src/main/java/edu/cascadia/brianb/mydrawing/SimplyDrawnView.java
+++ app/src/main/java/edu/cascadia/brianb/mydrawing/SimplyDrawnView.java
@@ -7,9 +7,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
-import android.graphics.PathDashPathEffect;
 import android.graphics.RectF;
-import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -43,19 +41,47 @@ public class SimplyDrawnView extends View {
         canvas.drawLine(mWidth, 0, 200, mHeight, mPaint);
 
         //Draw red line
-        mPaint.setColor(Color.RED); mPaint.setStrokeWidth(10);
+        mPaint.setColor(Color.RED);
+        mPaint.setStrokeWidth(10);
+        canvas.drawLine(mWidth / 2, mHeight / 2, mWidth, mHeight, mPaint);
 
         //Draw green lines
-
+        mPaint.setColor(Color.GREEN);
+        final int SPACER = ((int) ((mHeight / 2) / 4) - 25); // int for spacing on lines
+        for (int idx = 0; idx < 5; ++idx) {
+            mPaint.setAlpha(255 - (idx * 60));
+            canvas.drawLine(0, (idx * SPACER) + 25, mWidth, (idx * SPACER) + 25, mPaint);
+        }
 
         //Draw Text
+        mPaint.setColor(Color.BLACK);
+        mPaint.setAntiAlias(true);
+        mPaint.setTextSize(45);
+        canvas.drawText(getResources().getString(R.string.drawingText), 0, mHeight / 2, mPaint);
 
         //Draw Text on a Path
+        mPaint.setColor(Color.CYAN);
+        RectF rect = new RectF(mWidth / 3, (mHeight / 2 ) - 185, mWidth - 25, (mHeight / 2) + 185);
+        mPath.addOval(rect, Path.Direction.CW);
+        canvas.drawTextOnPath(getResources().getString(R.string.pathText), mPath, 950, 20, mPaint);
 
         //Draw filled, opaque, and open ovals
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setColor(Color.RED);
+        canvas.drawCircle((mWidth / 2), (mHeight / 2), 15, mPaint);
+        mPaint.setColor(Color.LTGRAY);
+        mPaint.setAlpha(185);
+        canvas.drawCircle((mWidth / 2), (mHeight / 2), 155, mPaint);
+        mPaint.setColor(Color.BLACK);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(10);
+        canvas.drawCircle((mWidth / 2), (mHeight / 2), 160, mPaint);
 
         //Draw bee bitmap
-        Drawable b = getResources().getDrawable(R.drawable.bee,null);
+        Bitmap bee = BitmapFactory.decodeResource(getResources(), R.drawable.bee);
+        bee = bee.createScaledBitmap(bee, bee.getWidth() * 2, bee.getHeight() * 2, false);
+        canvas.drawBitmap(bee, 0, 0, null);
+        // Drawable b = getResources().getDrawable(R.drawable.bee,null);
 
     }
 }
